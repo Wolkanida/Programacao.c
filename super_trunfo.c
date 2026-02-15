b6545d72a1de7056301805eb2348cd9509f51f86
Vitor <vitorvillelaedf@gmail.com>
2026-02-14 21:52:22 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+// Trabalho jogo super Trunfo
+// declaração de variaveis para as cartas.
+
+int main(){
+    
+// Carta 1 variaveis.
+    char estado1;
+    char codigo1 [5];
+    char cidade1 [50];
+    int  populacao1;
+    float area1;
+    float pib1;
+    int  pturi1;
+
+// Carta2 Variaveis.
+    char estado2;
+    char codigo2 [5];
+    char cidade2 [50];
+    int  populacao2;
+    float area2;
+    float pib2;
+    int  pturi2;
